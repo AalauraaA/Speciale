389d062079cc6f832e5bd2c9a7ed10d219b6b8cc
AalauraaA <laura.nyrup.mogensen@gmail.com>
2019-09-25 07:36:37 +0200

    L

--- Python_kode/Cov_DL.py
+++ Python_kode/Cov_DL.py
@@ -8,31 +8,34 @@ Topic: Covariance-Domain Dictionary Learning
 """
 import numpy as np
 
-Y = 0 #EEG data matrix size M x Nd
-S_f = 100 # Sample frequency in Hz
-Y_s = 0 # Overlapping segment of EEG data matrix. Size M x ts S_f
-ts = 2 # time segment in seconds ts \in [2,4], 200 frames
-Nd = 0
-
-# 
+Sf = 100            # Sample frequency in Hz
+ts = 2              # Time segment lenght in seconds ts \in [2,4], 200 frames
+Duration = 66       # Minutes
+S = (Duration*60)/2 # Time segment in seconds
+ 
+M = 32              # Sensor
 """
-Linear Mixture Model Ys = A Xs for all s
-with Ys Ys^T = A Xs Xs^T A^T and sample data covariance
-Sigma_Ys = 1/Ls Ys Ys^T for each segment s
+Linear Mixture Model:
+    Ys = A * Xs
 """
-def Cov_DL(N, M, Ls):
-    Xs = 0 # Source matrix for time segment s
-    Sigma_Xs = 1/Ls * Xs * Xs.T
-
-    delta_s = np.diag(Sigma_Xs)
+def Cov_DL(N, M, Ls, X):
+    Xs = X # Source matrix for time segment s
+    Sigma_X = 1/Ls * Xs * Xs.T       # Source Sample Covariance
+    Sigma_Y = np.zeros((M, ts * Sf)) # Sample Data Covariance
     D = 0  # Dictionary of size M(M+1)/2 x N -- BiGAMP-DL
-    for i in range(N):
-        D[i] = a[i] * a[i].T
+    a = np.zeros(N) #
+    for s in range(S):
+        for i in range(N):
+            delta = np.diag(Sigma_X)
+            E, V = np.linalg.eig(np.linalg.inv(D[i])) 
+            E1 = E[0] # Largest eigenvalue
+            V1 = V[0] # Associated eigenvector
+            
+            a[i] = np.sqrt(E1) * V1 # Global minimum of optimisation problem
+            
+            Sigma_Y[s] = delta[i][s] * a[i] * a[i].T + Err[s]
+    
+    
 
-    Sigma_Ys = D * delta_s + Es
-    
-    Lambda = 0
-    for i in range(N):
-        Sigma_Ys[i] = Lambda_s[i][i] * D[i] + Es[i] # D[i] = a[i] * a[i].T
-    
-    Sigma_Ys = A * Lambda_s * A.T + Es
+#    Sigma_Ys = D * delta_s + Es
+#    Sigma_Ys = A * Lambda_s * A.T + Es

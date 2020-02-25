126c697fac97fb1f4d2868f281eb17b374fd5b47
Trine jensen <Trine Nyholm Jensen>
2020-02-25 10:51:38 +0100

    T

--- Python_kode/Optimeringsproblem/opt_ex.py
+++ Python_kode/Optimeringsproblem/opt_ex.py
@@ -30,72 +30,67 @@ np.random.seed(5)
 # Generate cost function 
 
 #def D(X):
-#    D = np.zeros([int(X.shape[0]*2),X.shape[1]])
-#    for i in range(X.shape[1]): # run over N
-##        d = np.outer(X.T[i],X.T[i])
-##        d_vec = d[np.tril_indices(X.shape[0])]
-#        d_vec = np.hstack((X.T[i],X.T[i]))
+#    D = np.zeros([int(len(X)*(len(X)+1)/2.),len(X.T)])
+#    for i in range(len(X.T)): # run over N
+#        d = np.outer(X.T[i],X.T[i])
+#        d_vec = d[np.tril_indices(len(X.T[i]))]
 #        D.T[i] = d_vec
 #    return D
-#
+
+
 #def D(X):
-#    D = np.zeros([int(X.shape[0]*2),X.shape[1]])
+#    D = np.zeros([int(X.shape[0]),X.shape[1]])
 #    for i in range(X.shape[1]): # run over N
-##        d = np.outer(X.T[i],X.T[i])
-##        d_vec = d[np.tril_indices(X.shape[0])]
 #        d_vec = X.T[i]
 #        D.T[i] = d_vec
-#
-#m = 3
-#n = 4
-#
-#U = np.random.randint(1,10,size=(m,n))
-#print(U)
-#
-#
-## Define and solve the CVXPY problem.
-#A = cp.Variable((m,n))
-#cost = cp.sum_squares(D(A) - U)
-#
-##constraints = [D(A) == A]
-##constraints += [
-##    D[i] * A == b[i] for i in range(m)
-##]
-#
-#prob = cp.Problem(cp.Minimize(cost))
-#prob.solve()
-#
-## Print result.
-#print("\nThe optimal value is", prob.value)
-#print("The optimal x is")
-#print(A.value)
-#print("The norm of the residual is ", cp.norm(A - U, p=2).value)
+
+m = 3
+n = 4
+
+U = np.random.randint(1,10,size=(m,m))
+print(U)
+
+# Define and solve the CVXPY problem.
+D = cp.Variable((m,m))
+A = cp.Variable((m,m))
+cost = cp.sum_squares(D - U)
+
+constraints = [D[i] == np.outer(A.T[i],A.T[i])[np.tril_indices(m)] for i in range(m)]
+
+prob = cp.Problem(cp.Minimize(cost), constraints)
+prob.solve()
+
+# Print result.
+print("\nThe optimal value is", prob.value)
+print("The optimal x is")
+print(D.value)
+print("The norm of the residual is ", cp.norm(D - U, p=2).value)
 
 ## augmented lagrangian exsempel 
 
-np.random.seed(1)
-
-# Initialize data.
-MAX_ITERS = 10
-rho = 1.0
-n = 20
-m = 10
-A = np.random.randn(m,n)
-b = np.random.randn(m)
-
-# Initialize problem.
-x = cp.Variable(shape=n)
-f = cp.norm(x, 1)
-
-# Solve with CVXPY.
-cp.Problem(cp.Minimize(f), [A*x == b]).solve()
-print("Optimal value from CVXPY: {}".format(f.value))
-
-# Solve with method of multipliers.
-resid = A*x - b
-y = cp.Parameter(shape=(m)); y.value = np.zeros(m)
-aug_lagr = f + y.T*resid + (rho/2)*cp.sum_squares(resid)
-for t in range(MAX_ITERS):
-    cp.Problem(cp.Minimize(aug_lagr)).solve()
-    y.value += rho*resid.value
-    
+#np.random.seed(1)
+#
+## Initialize data.
+#MAX_ITERS = 10
+#rho = 1.0
+#n = 20
+#m = 10
+#A = np.random.randn(m,n)
+#b = np.random.randn(m)
+#
+## Initialize problem.
+#x = cp.Variable(shape=n)
+#f = cp.norm(x, 1)
+#
+## Solve with CVXPY.
+#cp.Problem(cp.Minimize(f), [A*x == b]).solve()
+#print("Optimal value from CVXPY: {}".format(f.value))
+#
+## Solve with method of multipliers.
+#resid = A*x - b
+#y = cp.Parameter(shape=(m)); y.value = np.zeros(m)
+#aug_lagr = f + y.T*resid + (rho/2)*cp.sum_squares(resid)
+#for t in range(MAX_ITERS):
+#    cp.Problem(cp.Minimize(aug_lagr)).solve()
+#    y.value += rho*resid.value
+#    

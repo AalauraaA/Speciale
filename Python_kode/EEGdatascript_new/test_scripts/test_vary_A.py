f95225fe58c81bdfa8db47d3bc2483c0f1c1c464
AalauraaA <laura.nyrup.mogensen@gmail.com>
2020-04-27 11:15:37 +0200

    L

--- Python_kode/EEGdatascript_new/test_scripts/test_vary_A.py
+++ Python_kode/EEGdatascript_new/test_scripts/test_vary_A.py
@@ -13,38 +13,52 @@ from simulated_data import generate_AR
 from simulated_data import MSE_one_error
 import numpy as np
 import matplotlib.pyplot as plt
+import data
 
 #np.random.seed(4523)
 
-M = 8
-N = 16
+N = 16  
 k = 16
 L = 1000 
 n_seg = 1
 
-Y, A_real, X_real = generate_AR(N, M, L, k)
-X_real = X_real.T[:-2]
-X_real = X_real.T
+iterationer = 3
+Xmse_list = np.zeros((4,iterationer))
+Amse_list = np.zeros((4,iterationer))
 
-A = [vary_A.A_uniform(M,N), vary_A.A_random(M,N), vary_A.A_gaussian(M,N), vary_A.EEG_A(M,N)]
+Xmse = np.zeros(4)
+Amse = np.zeros(4)
 
-Xmse_list = np.zeros(10)
-Amse_list = np.zeros(10)
+for ite in range(iterationer):
+    print('iteration {}'.format(ite))
 
-Xmse = np.zeros(len(A))
-Amse = np.zeros(len(A))
-
-for i in range(len(A)):
-    print(i)
-    for ite in range(10):
-        X_result = Main_Algorithm(Y, A[i], M, N, k, L, n_seg)
-        Xmse_list[ite] = MSE_one_error(X_real,X_result)  
-        Amse_list[ite] = MSE_one_error(A_real,A[i])
-
-        
+    # M=N
+    Y_f, A_real_f, X_real = generate_AR(N, N, L, k) #(N,M) is real
     
-    Xmse[i] = np.average(Xmse_list)  
-    Amse[i] = np.average(Amse_list)
+    #reduce so M<N
+    request='remove 1/2'
+    Y = data._reduction(Y_f, request)
+    A_real = data._reduction(A_real_f, request)
+    
+    M = len(Y)
+    
+    X_real = X_real.T[:-2]
+    X_real = X_real.T
+    
+    A = [vary_A.A_uniform(M,N), vary_A.A_random(M,N), vary_A.A_gaussian(M,N), vary_A.A_ICA(Y_f,request)]
+
+    for i in range(len(A)):
+        print('A_fix type {}'.format(i))
+    
+        X_result = Main_Algorithm(Y, A[i], M, N, k, L, n_seg)
+        
+        Xmse_list[i][ite] = MSE_one_error(X_real,X_result)  
+        Amse_list[i][ite] = MSE_one_error(A_real,A[i])
+
+           
+for j in range(len(Xmse)):
+    Xmse[j] = np.average(Xmse_list[j])  
+    Amse[j] = np.average(Amse_list[j])
 
 
 """ PLOTS """
@@ -55,10 +69,12 @@ plt.plot(0, Amse[0], 'ro')
 plt.plot(1, Amse[1], 'ro')
 plt.plot(2, Amse[2], 'ro')
 plt.plot(3, Amse[3], 'ro')
+
 plt.plot(0, Xmse[0], 'bo')
 plt.plot(1, Xmse[1], 'bo')
 plt.plot(2, Xmse[2], 'bo')
 plt.plot(3, Xmse[3], 'bo')
+
 plt.title('MSE of varying A')
 plt.xticks([])
 plt.ylabel('MSE')

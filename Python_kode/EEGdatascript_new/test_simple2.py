779cc2ea6ac5d354b0dee0dd5d9ca2183e92537d
Trine jensen <Trine Nyholm Jensen>
2020-06-02 16:35:40 +0200

    T

--- Python_kode/EEGdatascript_new/test_simple2.py
+++ Python_kode/EEGdatascript_new/test_simple2.py
@@ -37,52 +37,52 @@ print('MSE_A_0 = {}'.format(np.round_(A_mse0,decimals=3)))
 print('MSE_X = {}'.format(np.round_(mse_avg,decimals=3)))
 
 ##### plot
-#segment = 0
-#plt.figure(0)
-#models = [X_real[segment], Y[segment]]
-#names = ['Source Signals, $\mathbf{X}$',
-#         'Measurements, $\mathbf{Y}$',
-#         ]
-#colors = ['red', 'steelblue', 'orange', 'green', 'yellow', 'blue', 'cyan',
-#          'purple']
-#
-#for ii, (model, name) in enumerate(zip(models, names), 1):
-#    plt.subplot(2, 1, ii)
-#    plt.title(name)
-#    for sig, color in zip(model, colors):
-#        plt.plot(sig, color=color)
-#
-#plt.tight_layout()
-#plt.xlabel('sample')
-#plt.tight_layout()
-#plt.show()
-#plt.savefig('figures/simple_data.png')
-#
-#plt.figure(1)
-#plt.title('Comparison of Mixing Matrix - COV-DL2')
-#plt.plot(np.reshape(A_real, (A_real.size)), 'o-g',label=r'True $\mathbf{A}$')
-#plt.plot(np.reshape(A_result[segment], (A_result[segment].size)),'o-r', label=r'Estimate $\hat{\mathbf{A}}$')
-#plt.plot(np.reshape(A_init,(A_init.size)), 'o-b',label=r'init $\mathbf{A}$')
-#plt.legend()
-#plt.xlabel('index')
-#plt.show
-#plt.savefig('figures/COV2_simple.png')
-#
-#plt.figure(2)
-#plt.title('Source matrix - M-SBL')
-#nr_plot=0
-#for i in range(len(X_real.T[0])):
-#    if np.any(X_real[segment][i]!=0) or np.any(X_result[segment][i]!=0):
-#        
-#        nr_plot += 1
-#        plt.subplot(k, 1, nr_plot)
-#       
-#        plt.plot(X_real[segment][i], 'g',label='Real X')
-#        plt.plot(X_result[segment][i],'r',label='Recovered X')
-#
-#
-#plt.legend()
-#plt.xlabel('sample')
-#plt.tight_layout()
-#plt.show
-#plt.savefig('figures/M-SBL_simple1.png')
+segment = 0
+plt.figure(0)
+models = [X_real[segment], Y[segment]]
+names = ['Source Signals, $\mathbf{X}$',
+         'Measurements, $\mathbf{Y}$',
+         ]
+colors = ['red', 'steelblue', 'orange', 'green', 'yellow', 'blue', 'cyan',
+          'purple']
+
+for ii, (model, name) in enumerate(zip(models, names), 1):
+    plt.subplot(2, 1, ii)
+    plt.title(name)
+    for sig, color in zip(model, colors):
+        plt.plot(sig, color=color)
+
+plt.tight_layout()
+plt.xlabel('sample')
+plt.tight_layout()
+plt.show()
+plt.savefig('figures/simple_data.png')
+
+plt.figure(1)
+plt.title('Comparison of Mixing Matrix - COV-DL2')
+plt.plot(np.reshape(A_real, (A_real.size)), 'o-g',label=r'True $\mathbf{A}$')
+plt.plot(np.reshape(A_result[segment], (A_result[segment].size)),'o-r', label=r'Estimate $\hat{\mathbf{A}}$')
+plt.plot(np.reshape(A_init,(A_init.size)), 'o-b',label=r'init $\mathbf{A}$')
+plt.legend()
+plt.xlabel('index')
+plt.show
+plt.savefig('figures/COV2_simple.png')
+
+plt.figure(2)
+plt.title('Source matrix - M-SBL')
+nr_plot=0
+for i in range(len(X_real.T[0])):
+    if np.any(X_real[segment][i]!=0) or np.any(X_result[segment][i]!=0):
+        
+        nr_plot += 1
+        plt.subplot(k, 1, nr_plot)
+       
+        plt.plot(X_real[segment][i], 'g',label='Real X')
+        plt.plot(X_result[segment][i],'r',label='Recovered X')
+
+
+plt.legend()
+plt.xlabel('sample')
+plt.tight_layout()
+plt.show
+plt.savefig('figures/M-SBL_simple1.png')
